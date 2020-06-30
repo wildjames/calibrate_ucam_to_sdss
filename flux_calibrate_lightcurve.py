12bb3544d00d668f99a59527ee717e5a33547b4c
James Wild <jwild2@sheffield.ac.uk>
2020-06-30 13:20:39 +0100

    incrimental update

--- flux_calibrate_lightcurve.py
+++ flux_calibrate_lightcurve.py
@@ -21,8 +21,8 @@ from calphot.getEclipseTimes import tcorrect
 # Aperture 1 is assumed to be the target star!
 #fname = 'data/run015.log'
 # fname = 'Quality_Reductions/2019_09_27-run015.log'
-fname = 'Quality_Reductions/2019_09_29-run015.log'
-# fname = 'Quality_Reductions/2019_09_30-run013.log'
+# fname = 'Quality_Reductions/2019_09_29-run015.log'
+fname = 'Quality_Reductions/2019_09_30-run013.log'
 oname = 'ASASSN-17jf'
 lc_dir = 'LIGHTCURVES'
 
