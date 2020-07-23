a2de80be6056f54aff1d97e431f6331376d56e5a
James Wild <jwild2@sheffield.ac.uk>
2020-07-23 15:47:15 +0100

    Moving over to more usable versions

--- generate_colour_term_gradients.py
+++ generate_colour_term_gradients.py
@@ -22,7 +22,7 @@ if os.path.isfile(variables_fname):
 if not os.path.isdir("figs"):
     os.mkdir('figs')
 
-mydir = os.abspath(__file__)
+mydir = os.path.abspath(__file__)
 
 # These are not relevant - leave as zero for all bands
 k_ext = {
